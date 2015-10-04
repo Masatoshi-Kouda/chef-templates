8d5e89f95a757da0a9f9f96a89c7e2a6eea455c0
Masatoshi Kouda <masatoshi.kouda@gmail.com>
2015-10-05 07:10:43 +0900

    add Dockerfile, jenkins build.sh

---
+++ jenkins/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+####################
+# FUNCTION
+####################
+ssh_config () {
+    local port=$1
+    cat << EOF
+Host app
+  HostName 172.17.42.1
+  User docker
+  Port ${port}
+  UserKnownHostsFile /dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  IdentityFile "$WORKSPACE/docker/id_rsa"
+  IdentitiesOnly yes
+  LogLevel FATAL
+EOF
+}
+
+node_json () {
+    cat << EOF
+{
+  "run_list": [
+    "recipe[base]"
+  ]
+}
+EOF
+}
+
+####################
+# MAIN
+####################
+bundle install --path vendor/bundle
+
+cd $WORKSPACE/docker
+sudo docker images | grep -q centos/sshd
+if [ $? -ne 0 ]; then
+    sudo rm -f ./id_rsa*
+    sudo -u jenkins ssh-keygen -t rsa -C '' -f ./id_rsa -N ''
+    sudo docker build -t centos/sshd .
+fi
+
+sudo docker run -d -P --name test_sshd centos/sshd
+port=$(sudo docker inspect --format='{{(index (index .NetworkSettings.Ports "22/tcp") 0).HostPort}}' test_sshd)
+ssh_config $port > ~/.ssh/ssh_config
+
+node_json > $WORKSPACE/chef/nodes/app.json
+
+sleep 3
+cd $WORKSPACE/chef
+bundle exec knife solo bootstrap app -F ~/.ssh/ssh_config
+
+sudo docker stop test_sshd
+sudo docker rm test_sshd
