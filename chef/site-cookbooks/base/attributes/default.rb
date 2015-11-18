ef706779aa2fe8b187b41871a03929e9c3a18fda
Masatoshi Kouda <masatoshi.kouda@gmail.com>
2015-11-19 06:50:23 +0900

    update site-cookbooks base

--- chef/site-cookbooks/base/attributes/default.rb
+++ chef/site-cookbooks/base/attributes/default.rb
@@ -3,5 +3,5 @@ default['epel']['centos7']['url_path'] = 'http://dl.fedoraproject.org/pub/epel/7
 default['remi']['centos6']['url_path'] = 'http://rpms.famillecollet.com/enterprise/remi-release-6.rpm'
 default['remi']['centos7']['url_path'] = 'http://rpms.famillecollet.com/enterprise/remi-release-7.rpm'
 default['rpmforge']['gpg_key'] = 'http://apt.sw.be/RPM-GPG-KEY.dag.txt'
-default['rpmforge']['centos6']['url_path'] = 'http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm'
+default['rpmforge']['centos6']['url_path'] = 'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm'
 default['rpmforge']['centos7']['url_path'] = 'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm'
