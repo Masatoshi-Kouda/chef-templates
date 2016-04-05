98b272eace6a2d4576ed3596475793ce3ff403df
Masatoshi-Kouda <masatoshi.kouda@gmail.com>
2016-04-05 23:51:28 +0900

    Update recipe hostname

--- chef/site-cookbooks/hostname/recipes/default.rb
+++ chef/site-cookbooks/hostname/recipes/default.rb
@@ -3,15 +3,32 @@ execute "hostname-setting" do
   action :nothing
 end
 
-template "/etc/sysconfig/network" do
-  source "network.erb"
-  mode 0644
-  owner "root"
-  group "root"
-  variables({
-    :hostname => node[:host_name]
-  })
-  notifies :run,"execute[hostname-setting]",:immediately
-  notifies :restart, 'service[rsyslog]'
+service "rsyslog" do
+  action [ :enable, :start]
 end
 
+if node['platform_version'].to_i == 6 || node["platform"] == "amazon"
+  template "/etc/sysconfig/network" do
+    source "network.erb"
+    mode 0644
+    owner "root"
+    group "root"
+    variables({
+      :hostname => node[:host_name]
+    })
+    notifies :run,"execute[hostname-setting]",:immediately
+    notifies :restart, 'service[rsyslog]'
+  end
+elsif node['platform_version'].to_i == 7
+  template "/etc/hostname" do
+    source "hostname.erb"
+    mode 0644
+    owner "root"
+    group "root"
+    variables({
+      :hostname => node[:host_name]
+    })
+    notifies :run,"execute[hostname-setting]",:immediately
+    notifies :restart, 'service[rsyslog]'
+  end
+end
